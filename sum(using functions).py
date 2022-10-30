cd23362360bb623434db037123445178c81de0f2
jappan-s <63873951+jappan-s@users.noreply.github.com>
2022-10-30 23:40:52 +0530

    Add files via upload

---
+++ sum(using functions).py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+
+def sumlist(list):
+    total = 0
+    for i in range(len(list)):
+        total = total+list[i]
+    return total
+list=[8,2,3,0,7]
+print(list)
+print("sum of list is : ",sumlist(list))
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
